97b03aed0e2723beb19fd3819d03f09fa8db5573
Tiernan Garsys <tgarsys@mac.com>
2021-01-24 18:04:03 -0500

    refactor directory layout

---
+++ src/components/Home.tsx
@@ -0,0 +1,34 @@
+import './App.css';
+
+function Home() {
+  return (
+    <div className="App">
+      <div className="cell">
+        <div className="subtitle">
+          Welcome to the internet homepage of
+        </div>
+        <div className="title">
+          Tiernan Garsys
+        </div>
+      </div>
+      <div className="cell">
+        <div className="subtitle">
+          About
+        </div>
+      </div>
+      <div className="cell">
+        <div className="subtitle">
+          Code
+        </div>
+      </div>
+      <div className="cell">
+        <div className="subtitle">
+          Music
+        </div>
+      </div>
+    </div>
+    
+  );
+}
+
+export default Home;
